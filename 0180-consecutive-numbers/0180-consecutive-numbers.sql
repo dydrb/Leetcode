72b154e97e9fb230a93e2f91746cda60b0d3c509
dydrb <105268330+dydrb@users.noreply.github.com>
2023-03-13 10:42:36 +0900

    Time: 938 ms (65.88%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select distinct Num as ConsecutiveNums
 from Logs
-where (Id + 1, Num) in (select * from Logs) 
-                        and (Id + 2, Num) in (select * from Logs)
+where (Id + 1, Num) in (select * from Logs)    # 다음 숫자 판단
+                        and (Id + 2, Num) in (select * from Logs) # 그 다음 숫자 판단
